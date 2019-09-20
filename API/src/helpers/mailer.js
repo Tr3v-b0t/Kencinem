40ba04f9fb75efd23c712a48e4428ad2c7a96a8b
b0nbon1 <nyabuyabonvic@gmail.com>
2019-09-20 08:24:05 -0700

    feature(login): - login user - reset user password - updated password [finishes #168309382]

--- API/src/helpers/mailer.js
+++ API/src/helpers/mailer.js
@@ -12,4 +12,15 @@ const Mailer = async (to, tokenLink) => {
         html: "<strong>welcome to kenCinema</strong> <p> click the link below to activate your account </p>" +` ${tokenLink} `
     });
 };
-export default Mailer;
+
+const ResetMail = async (to, tokenLink) => {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    await sgMail.send({
+        to,
+        from: process.env.email,
+        subject: "Reset password",
+        html: "<strong>welcome to kenCinema</strong> <p> click the link below to reset your password</p>" +` ${tokenLink} `
+    });
+};
+
+export { Mailer, ResetMail };
